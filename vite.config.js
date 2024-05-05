912deb4d5a18ef5ae743fd93fa8612d1a58dc13c
GabLocal <gabrielborcena@gmail.com>
2024-05-05 22:05:14 +0800

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PokeManiav2/' // This should be the name of your GitHub repository
+  base: '/PokeMania/' // This should be the name of your GitHub repository
 })
